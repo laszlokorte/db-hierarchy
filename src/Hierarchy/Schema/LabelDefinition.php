372eed1c02247731c7c12be12721d7bdbac8dc36
Laszlo Korte <git@laszlokorte.de>
2021-06-17 03:26:48 +0200

    clean up templates

--- src/Hierarchy/Schema/LabelDefinition.php
+++ src/Hierarchy/Schema/LabelDefinition.php
@@ -6,10 +6,12 @@ class LabelDefinition {
 	private $singular;
 	private $plural;
 	private $description;
+	private $icon;
 
-	public function __construct($singular, $plural, $description) {
+	public function __construct($singular, $plural, $description, $icon) {
 		$this->singular = $singular;
 		$this->plural = $plural;
 		$this->description = $description;
+		$this->icon = $icon;
 	}
 }
